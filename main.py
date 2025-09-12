1a6f1ebfa1c3a3f44fd36e93a90e5d41167aeab5
KS Arya Devi <aryadevi2002@gmail.com>
2025-09-12 18:18:49 +0530

    stackoverflow method created

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ model=SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 faq_questions=list(faq.keys())
 faq_embeddings=model.encode(faq_questions,convert_to_tensor=True)
 class HybridPyChat:
-    def __init__(self,curated_threshold=0.65,web_search_enabled=True):
+    def __init__(self,curated_threshold=0.75,web_search_enabled=True):
         self.curated_threshold=curated_threshold
         self.web_search_enabled=web_search_enabled
         self.query_cache={}
@@ -100,8 +100,8 @@ class HybridPyChat:
             headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'}
             response=requests.get(url,headers=headers,timeout=10)
             response.raise_for_status()
-            soup=BeautifulSoup(response.content,'html,parser')
-            for script in soup(['scripts','style']):
+            soup=BeautifulSoup(response.content,'html.parser')
+            for script in soup(['script','style']):
                 script.decompose()
             text=soup.get_text()
             lines = (line.strip() for line in text.splitlines())
@@ -111,42 +111,127 @@ class HybridPyChat:
         except Exception as e:
             print(f"error extracting {url} with {e}")
             return ""
-    def search_web(self,query):
-        if not self.web_search_enabled:
-            return None
-        print("searching the web...")
-        if query in self.query_cache:
-            print("query found in cache")
-            return self.query_cache[query]
-        #code to search the web
-        search_query=f"{query} in python programming language "
-        encoded_query=quote(search_query)
-        browser_url=f"https://api.duckduckgo.com/?q={encoded_query}&format=json&no_html=1&skip_disambig=1"
-        response=requests.get(browser_url,timeout=10)
-        data=response.json()
-        answer_text=""
-        source_url=""
-        if data.get('AbstractText'):
-            answer_text=data['AbstractText']
-            source_url=data['AbstractURL']
-        elif data.get('Answer'):
-            answer_text=data['Answer']
-        elif data.get('Results') and len(data['Results'])>0:
-            first_result=data['Results'][0]
-            answer_text=first_result.get('Text','')
-            source_url=first_result.get('FirstURL','')
-        if source_url and len(answer_text)<200:
-            detailed_text=self.extract_from_url(source_url)
-            if detailed_text:
-                sentences=detailed_text.split('.')[:3]
-                answer_text='. '.join(sentences)+'.'
-        if answer_text and len(answer_text.strip()) > 50:
-            result = {
-                    'answer': answer_text.strip(),
-                    'source': 'web',
-                    'confidence': 0.7,  # Moderate confidence for web results
-                    'source_url': source_url
-                }
+    
+
+    def search_stackoverflow(self,query):
+        try:
+            print("searching stackoverflow...")
+            stack_url="https://api.stackexchange.com/2.3/search/advanced"
+            search_params={
+            "order": "desc",
+            "sort": "relevance",
+            "q": f"{query} in python",
+            "tagged": "python",
+            "site": "stackoverflow"}
+            print("making request to the url...")
+            search_results=requests.get(stack_url,search_params)
+            print(f"search url: {search_results.url}")
+            print(f"status code: {search_results.status_code}")
+            if search_results.status_code!=200:
+                print("API request failed with status code: ",search_results.status_code)
+                return None
+            search_data=search_results.json()
+            print(f"response keys {list(search_data.keys())}")
+            if 'error_message' in search_results:
+                print(f"api error {search_results['error_message']}")
+                return None
+            
+            if search_data.get('items') and len(search_data['items'])>0:
+                best_item=search_data['items'][0]
+                # print("printing best items",best_item)
+                if best_item.get('accepted_answer_id'):
+                    answer_url=f"https://api.stackexchange.com/2.3/answers/{best_item['accepted_answer_id']}"
+                    answer_params={'site': 'stackoverflow', 'filter': 'withbody'}
+                    answer_response=requests.get(answer_url,params=answer_params,timeout=10).json
+                    if answer_response.get('items'):
+                        answer_body=answer_response['items'][0].get('body','')
+                        #clean html
+                        soup=BeautifulSoup(answer_body,'html.parser')
+                        clean_answer=soup.get_text()[:1000]
+                        return{
+                            'answer':clean_answer,
+                            'source':'stack overflow',
+                            'confidence':0.7,
+                            'source_link':best_item.get('link','')
+                        }
+        except Exception as e:
+            print(f"searching failed {e}")
+
+
+        #duckduckgo
+    # def search_web(self,query):
+    #     if not self.web_search_enabled:
+    #         return None
+    #     print("searching the web...")
+    #     if query in self.query_cache:
+    #         print("query found in cache")
+    #         return self.query_cache[query]
+    #     #code to search the web
+    #     try:
+    #         search_query=f"{query} in python programming language "
+    #         print("searched")
+    #         encoded_query=quote(search_query)
+    #         print("encoded the query")
+    #         browser_url=f"https://api.duckduckgo.com/?q={encoded_query}&format=json&no_html=1&skip_disambig=1"
+    #         response=requests.get(browser_url,timeout=10)
+    #         # print("response kitti")
+    #         data=response.json()
+    #         print(data)
+    #         answer_text=""
+    #         source_url=""
+    #         # print("response kittiyo?")
+           
+    #         if data.get('AbstractText'):
+    #             print("Found text")
+    #             answer_text=data['AbstractText']
+    #             source_url=data['AbstractURL']
+    #         elif data.get('Answer'):
+    #             answer_text=data['Answer']
+    #         elif data.get('Results') and len(data['Results'])>0:
+    #             first_result=data['Results'][0]
+    #             answer_text=first_result.get('Text','')
+    #             source_url=first_result.get('FirstURL','')
+    #         if source_url and len(answer_text)<200:
+    #             detailed_text=self.extract_from_url(source_url)
+    #             if detailed_text:
+    #                 sentences=detailed_text.split('.')[:3]
+    #                 answer_text='. '.join(sentences)+'.'
+    #         if answer_text and len(answer_text.strip()) > 50:
+    #             result = {
+    #                     'answer': answer_text.strip(),
+    #                     'source': 'web',
+    #                     'confidence': 0.7,  # Moderate confidence for web results
+    #                     'source_url': source_url
+    #                 }
+    #             self.query_cache[query]=result
+    #             print("Found web answer")
+    #             return result
+    #         else:
+    #             print("No web results found")
+    #             return None
+    #     except Exception as e:
+    #         print(f"error {e}")
+    #         return None
+        
+    def get_respone(self,query): #Main method
+        print(f"User query : {query}")
+        curated_result=self.search_curated_faq(query)
+        if curated_result:
+            print("---Result found from faq---")
+            return curated_result
+        stackoverflow_results=self.search_stackoverflow(query)
+        if stackoverflow_results:
+            print("---Result found from web---")
+            return stackoverflow_results
+bot=HybridPyChat(curated_threshold=0.75,web_search_enabled=True)
+test_query="What's the difference between dictionary and list"
+response=bot.get_respone(test_query)
+print(response)
+
+        
+
+        
+
        
 
 
